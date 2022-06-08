ce55f3f22272910df00a249f1a980bcdf0e8f3ff
tsvetkov <tsvetkov@krista.ru>
2022-06-08 11:05:10 +0300

    add templates

---
+++ templates/Python/data_preprocessing_template.py
@@ -0,0 +1,19 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Simple_Salary.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Подготовка данных. Основные мероприятия
+
+# 1. Отсутствующие данные
+
+# 2. Кодирование категориальных признаков. One Hot Encoding
+
+# 3. Разделение набора на обучаемую и тестовую выборку
+
+# 4. Нормализация данных
