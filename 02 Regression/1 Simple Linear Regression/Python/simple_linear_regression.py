4dbde8aa5403f7a177dc80f5c3e1a9078d63672f
Maxim Tsvetkov <tsv.mks@gmail.com>
2022-06-15 11:08:45 +0300

    add new files

---
+++ 02 Regression/1 Simple Linear Regression/Python/simple_linear_regression.py
@@ -0,0 +1,23 @@
+# Simple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+
+# Training the Simple Linear Regression model on the Training set
+
+# Predicting the Test set results
+
+# Visualising the Training set results
+
+# Visualising the Test set results
+
+# Evaluating Regression Model
---
+++ 02 Regression/1 Simple Linear Regression/R/simple_linear_regression.R
@@ -0,0 +1,16 @@
+# Simple Linear Regression
+
+# Importing the dataset
+dataset = read.csv('Salary_Data.csv')
+
+# Splitting the dataset into the Training set and Test set
+
+# Training the Simple Linear Regression model on the Training set
+
+# Predicting the Test set results
+
+# Visualising the Training set results
+
+# Visualising the Test set results
+
+# Evaluating Regression Model
